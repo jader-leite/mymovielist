2ce9b350cd27703ab7e79b7ed4f20000398b2c75
Jader Filho <jader.filho@redeunifique.com.br>
2025-04-30 13:32:26 -0300

    updating movie type to contemplate more infos

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,7 +1,32 @@
+interface Rating {
+  Source?: string;
+  Value?: string;
+}
+
 export interface Movie {
-  imdbID: string;
   Title: string;
-  Poster: string;
   Year: string;
-  Ratings: string;
+  Rated?: string;
+  Released?: string;
+  Runtime?: string;
+  Genre?: string;
+  Director?: string;
+  Writer?: string;
+  Actors?: string;
+  Plot?: string;
+  Language?: string;
+  Country?: string;
+  Awards?: string;
+  Poster: string;
+  Ratings?: Rating[];
+  Metascore?: string;
+  imdbRating?: string;
+  imdbVotes?: string;
+  imdbID: string;
+  Type?: string;
+  DVD?: string;
+  BoxOffice?: string;
+  Production?: string;
+  Website?: string;
+  Response?: string;
 }
