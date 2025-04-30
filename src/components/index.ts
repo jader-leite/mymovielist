15e6d20569657e73cd53b03510d2381bc88f15db
Jader Filho <jader.filho@redeunifique.com.br>
2025-04-30 13:36:42 -0300

    new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 import Carrousel from './carrousel/Carrousel';
+import { EmptyState } from './ui/empty-state';
 import Footer from './footer/Footer';
 import Header from './header/Header';
 import MovieCard from './movie-card/MovieCard';
+import { Tooltip } from './ui/tooltip';
 
-export { Carrousel, Footer, Header, MovieCard };
+export { Carrousel, EmptyState, Footer, Header, MovieCard, Tooltip };
