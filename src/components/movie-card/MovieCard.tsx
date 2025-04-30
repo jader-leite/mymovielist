f1d96cd5fc1eb4fc0e85b4071a76c6fd88ae1ea4
Jader Filho <jader.filho@redeunifique.com.br>
2025-04-30 13:35:12 -0300

    updating movie card component to new stylization

--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -1,22 +1,28 @@
 import { Box, Image, Text } from '@chakra-ui/react';
 import { Movie as MovieCardProps } from '@/types';
+import { useNavigate } from 'react-router-dom';
+
+import PosterNotFound from '../../assets/images/poster-not-found.png';
 
 const MovieCard = (props: MovieCardProps) => {
+  const navigate = useNavigate();
   return (
     <Box
-      key={props.imdbID}
       bg="purple.900"
-      p={4}
       borderRadius="md"
       _hover={{ transform: 'scale(1.05)', transition: '0.3s' }}
       m={2}
+      p={2}
+      onClick={() => navigate(`/movies/${props.imdbID}`)}
+      cursor="button"
     >
       <Image
-        src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/300x450?text=No+Image'}
+        src={props.Poster !== 'N/A' ? props.Poster : PosterNotFound}
         alt={props.Title}
         borderRadius="md"
-        mb={4}
+        w={'100%'}
       />
+
       <Text
         fontSize="lg"
         fontWeight="bold"
