e503fd8cab1b9e8e837b9846c93db86382e3ed87
Jader Filho <jader.filho@redeunifique.com.br>
2025-05-05 16:49:55 -0300

    adding toaster

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './App.css';
 import App from './App.tsx';
 import { Provider } from './components/ui/provider';
+import { Toaster } from './components/ui/toaster.tsx';
 
 const root = document.getElementById('root');
 
@@ -15,6 +16,7 @@ createRoot(root).render(
   <StrictMode>
     <Provider>
       <App />
+      <Toaster />
     </Provider>
   </StrictMode>,
 );
--- src/pages/Movies/movies-details/components/FavoriteButton.tsx
+++ src/pages/Movies/movies-details/components/FavoriteButton.tsx
@@ -3,6 +3,7 @@ import { Tooltip } from '../../../../components';
 import { MdOutlineStar, MdOutlineStarBorder } from 'react-icons/md';
 import { useMyMovieListStore } from '../../../../stores';
 import { Movie } from '@/types';
+import { toaster } from '../../../../components/ui/toaster';
 
 interface FavoriteButtonProps {
   id: string | undefined;
@@ -12,6 +13,20 @@ interface FavoriteButtonProps {
 const FavoriteButton = ({ id, movie }: FavoriteButtonProps) => {
   const { addFavorite, removeFavorite, favorites } = useMyMovieListStore();
   const isFavorite = favorites.some((favorite) => favorite.imdbID === id);
+
+  const handleClick = (movie: Movie) => {
+    if (isFavorite) {
+      removeFavorite(movie.imdbID);
+      toaster.success({
+        title: 'Removed from favorites',
+      });
+      return;
+    }
+    addFavorite(movie);
+    toaster.success({
+      title: 'Added to favorites',
+    });
+  };
   return (
     <Tooltip content={isFavorite ? 'Remove from favorites' : 'Add to favorites'}>
       <IconButton
@@ -20,7 +35,7 @@ const FavoriteButton = ({ id, movie }: FavoriteButtonProps) => {
         bg="purple.700"
         width={10}
         height={10}
-        onClick={() => (isFavorite ? removeFavorite(movie.imdbID) : addFavorite(movie))}
+        onClick={() => handleClick(movie)}
       >
         {isFavorite ? <MdOutlineStar color="white" /> : <MdOutlineStarBorder fill="white" />}
       </IconButton>
